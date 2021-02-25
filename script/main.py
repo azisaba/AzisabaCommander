9b733c7875780a74c21cf08c4bbba69029846806
testusuke <silverbro007@icloud.com>
2021-02-26 07:46:53 +0900

    Update main.py

--- script/main.py
+++ script/main.py
@@ -2,6 +2,7 @@
 ########################
 #   Project: AzisabaCommander
 #   Author: testusuke
+#   Class: Main
 #   Date: 2020/2/22
 ########################
 
@@ -9,25 +10,29 @@ import sys
 import os
 import discord
 import ConfigLoader
+import CommandExecutor
+import asyncio
 
 ####################
 # Config Loader 
 ####################
 path = sys.argv[1]
 cl = ConfigLoader.ConfigLoader(path)
-# discord token
-token = ''
-# command prefix
-prefix = ''
-
+# discord setting
+token = cl.token
+prefix = cl.prefix
+guild_id = cl.guild
+channel_id = cl.channel
 ####################
 # Discord
 ####################
 # build
 client = discord.Client()
 
-# events
+# CommandExecutor
+ce = CommandExecutor.CommandExecutor(client,cl.commandMap,prefix)
 
+# events
 @client.event
 async def on_ready():
     print('Login: success!')
@@ -38,22 +43,22 @@ async def on_message(message):
     if message.author.bot:
         return
     # guild
-    if not message.guild.id == guild_id:
+    if not message.guild.id == int(guild_id):
         return
     # channel
-    if not message.channel.id == channel_id:
+    if not message.channel.id == int(channel_id):
         return
     # check prefix
     if not message.content[0] == prefix:
         return
     
+    msg = message.content[1:]
     # get command label
-    args = message.content.split()
+    args = msg.split()
     print(str(args))
+    # 受け渡し
+    ce.onCommand(message,args)
     
-    
-    
-        
 
+# run
 client.run(token)
-print('launch!')
